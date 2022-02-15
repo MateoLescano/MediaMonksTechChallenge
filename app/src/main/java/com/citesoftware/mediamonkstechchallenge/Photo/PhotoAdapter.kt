ca05751be784c9fe4b682938efd304a1f75feadd
MateoLescano <mateolescano@gmail.com>
2022-02-15 13:51:15 -0300

    Expandir foto del item

--- app/src/main/java/com/citesoftware/mediamonkstechchallenge/Photo/PhotoAdapter.kt
+++ app/src/main/java/com/citesoftware/mediamonkstechchallenge/Photo/PhotoAdapter.kt
@@ -5,8 +5,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.citesoftware.mediamonkstechchallenge.R
+import com.google.android.material.card.MaterialCardView
 import com.squareup.picasso.Picasso
 
 class PhotoAdapter(private val photoList: MutableList<PhotoDataModel>): RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
@@ -15,11 +17,27 @@ class PhotoAdapter(private val photoList: MutableList<PhotoDataModel>): Recycler
 
         private val tvPhotoTitle: TextView = itemView.findViewById(R.id.tvPhotoTitle)
         private val thumbnail: ImageView = itemView.findViewById(R.id.ivThumbnail)
+        private val expanded: ImageView = itemView.findViewById(R.id.ivExpanded)
+        private val photoItem: MaterialCardView = itemView.findViewById(R.id.photoItem)
 
         fun bindView(photoList: PhotoDataModel){
             val texto = "${photoList.title} \n (foto ${photoList.id})"
             tvPhotoTitle.text = texto
             Picasso.get().load(photoList.thumbnailUrl).into(thumbnail)
+            Picasso.get().load(photoList.url).into(expanded)
+
+            photoItem.setOnClickListener {
+                if (!expanded.isVisible){
+                    tvPhotoTitle.visibility = View.INVISIBLE
+                    thumbnail.visibility = View.INVISIBLE
+                    expanded.visibility = View.VISIBLE
+                }else{
+                    tvPhotoTitle.visibility = View.VISIBLE
+                    thumbnail.visibility = View.VISIBLE
+                    expanded.visibility = View.INVISIBLE
+                }
+            }
+
         }
     }
 
